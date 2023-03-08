3490d83c03c49140f2c1f143eb0562410a33aacb
vikash <vikash@logicwind.co>
2023-03-08 16:18:05 +0530

    inital commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const runtimeCaching = require("next-pwa/cache")
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+})
 
+const nextConfig = withPWA({
+  // next config
+})
 module.exports = nextConfig
